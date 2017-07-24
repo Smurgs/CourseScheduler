7c1c21497c3d406e6e9be7781379a0f0f9e4789d
Stefan <Stefan.Murga@hotmail.com>
2017-07-24 18:42:47 -0400

    re-org file structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='CourseScheduler',
+    version='0.1',
+    packages=['CourseScheduler',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
