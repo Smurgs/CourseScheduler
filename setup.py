3ab5a5924e372ce0033413dae347b476ba68f100
Stefan <Stefan.Murga@hotmail.com>
2017-07-24 19:07:48 -0400

    add makefile, requirements, and setup file

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version='0.1',
     packages=['CourseScheduler',],
     license='MIT License',
+    description='Carleton University course scheduling tool',
     long_description=open('README.md').read(),
 )
